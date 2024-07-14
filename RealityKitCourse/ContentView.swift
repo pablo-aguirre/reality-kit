721971e696944350041206ed8e504da4ea1c5b1b
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-14 14:37:22 +0200

    Video Materials

--- RealityKitCourse/ContentView.swift
+++ RealityKitCourse/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import AVFoundation
 
 struct ContentView : View {
     var body: some View {
@@ -19,14 +20,20 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
         
-        context.coordinator.view = arView
-        context.coordinator.setup() 
+        let anchor = AnchorEntity(plane: .horizontal)
         
+        guard let url = Bundle.main.url(forResource: "video", withExtension: "mp4") else { fatalError("Video not found!") }
+        let player = AVPlayer(url: url)
+        
+        let material = VideoMaterial(avPlayer: player)
+        material.controller.audioInputMode = .spatial // for video with audio
+        
+        let model = ModelEntity(mesh: .generatePlane(width: 0.5, depth: 0.5), materials: [material])
+        player.play()
+        
+        anchor.addChild(model)
+        arView.scene.addAnchor(anchor)
         return arView
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
