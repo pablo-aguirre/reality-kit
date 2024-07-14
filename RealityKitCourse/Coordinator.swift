3408cc82013177c0c4b1d1f002aa59dfc381bfc6
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-14 11:16:01 +0200

    Loading Multiple Models

--- RealityKitCourse/Coordinator.swift
+++ RealityKitCourse/Coordinator.swift
@@ -25,13 +25,21 @@ class Coordinator {
             let anchor = AnchorEntity(raycastResult: result)
             
             cancellable = ModelEntity.loadAsync(named: "robot")
+                .append(ModelEntity.loadAsync(named: "sneaker"))
+                .collect()
                 .sink { loadCompletion in
                     if case let .failure(error) = loadCompletion {
                         print("Unable to load model \(error).")
                     }
                     self.cancellable?.cancel()
-                } receiveValue: { entity in
-                    anchor.addChild(entity)
+                } receiveValue: { entities in
+                    var x: Float = 0.0
+                    
+                    entities.forEach { entity in
+                        entity.position = simd_make_float3(x, 0, 0)
+                        anchor.addChild(entity)
+                        x += 0.3
+                    }
                 }
             
             view.scene.addAnchor(anchor)
