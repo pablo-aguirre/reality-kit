b069c9a0d06314dcb42e4a7443882028814d531d
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-07-15 14:59:17 +0200

    AR Coaching Overlay

--- RealityKitCourse/ContentView.swift
+++ RealityKitCourse/ContentView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+extension ARView: ARCoachingOverlayViewDelegate {
+    
+    func addCoaching() {
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.goal = .horizontalPlane
+        coachingOverlay.session = self.session
+        coachingOverlay.delegate = self
+        self.addSubview(coachingOverlay)
+    }
+    
+    private func addVirtualObject() {
+        guard let anchor = self.scene.anchors.first(where: { $0.name == "Plane Anchor" }) else { return }
+        let boxModel = ModelEntity(mesh: .generateBox(size: 0.1), materials: [SimpleMaterial()])
+        anchor.addChild(boxModel)
+    }
+    
+    public func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        addVirtualObject()
     }
 }
 
@@ -19,13 +42,12 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
         
-        context.coordinator.view = arView
-        context.coordinator.buildEnvironment()
+        let anchor = AnchorEntity(plane: .horizontal)
+        anchor.name = "Plane Anchor"
+        arView.scene.addAnchor(anchor)
+        arView.addCoaching()
+        
         return arView
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
